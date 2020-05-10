8561cb29f2f1b15f87f3c2dc88fa442082d6e784
“b2” <mizukake213@gmail.com>
2020-05-10 17:14:52 +0900

    js commit

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+{
+  document.getElementById("title").addEventListener("click", () => {
+    alert("ようこそ！！！");
+  });
+}
+{
+  document.querySelector("li").addEventListener("click", () => {
+    confirm("下にあるから見てみてね");
+  });
+}
